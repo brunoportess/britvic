946903ef555f0ae61d74f1b10f646c27557bbdca
brunoportess <bruno.portess@gmail.com>
2021-01-28 11:00:58 -0300

    correcao nome tabela seed

--- database/seeders/ReservasSeeder.php
+++ database/seeders/ReservasSeeder.php
@@ -16,7 +16,7 @@ class ReservasSeeder extends Seeder
     public function run()
     {
         $datetime = Carbon::now();
-        DB::table('usuarios')->insert([
+        DB::table('veiculos_reservas')->insert([
             ['veiculo_id' => 1, 'usuario_id' => 1, 'data_inicio' => '2021-01-20', 'data_fim' => '2021-01-23', 'created_at' => $datetime, 'updated_at' => $datetime],
             ['veiculo_id' => 2, 'usuario_id' => 1, 'data_inicio' => '2021-01-25', 'data_fim' => '2021-01-29', 'created_at' => $datetime, 'updated_at' => $datetime],
             ['veiculo_id' => 3, 'usuario_id' => 2, 'data_inicio' => '2021-01-20', 'data_fim' => '2021-01-23', 'created_at' => $datetime, 'updated_at' => $datetime]
